b82898545acc7b3276e055169bd3ecf69aee11eb
LorenzoSansone <lorenzo.sansone3@studio.unibo.it>
2026-01-28 00:13:13 +0100

    while and for

---
+++ basic/loop_for.py
@@ -0,0 +1,24 @@
+
+
+#First solution
+"""
+for i in [0,1,2]:
+    print("Meow")
+"""
+    
+#Better way
+"""
+for i in range(3):
+    print("Meow")
+"""
+#Variable "_"
+"""
+for _ in range(3): #If we don't use the variable in the loop
+    print("Meow")
+"""
+
+#Moltiplication operator
+"""
+print("meow\n" * 3 , end = "") #It's less readible than before
+"""
+
--- basic/loops_while.py
+++ basic/loops_while.py
@@ -5,8 +5,7 @@ print("Meow")
 print("Meow")
 """
 
-#While
-
+#While with DECREMENT
 """
 i = 3
 while i != 0:
@@ -14,12 +13,16 @@ while i != 0:
     i -= 1 #i = i - 1
 """
 
+#While with Increment
 i = 0 #common to start from 0
 while i < 3:
     print("Meow")
-    i += 1 #i = i - 1
+    i += 1 #i = i + 1
 
+#INCREMENT MORE THAN 1
+"""
 i = 0
 while i <= 40:
     print(f"{i}")
     i +=10 #We can choose the incrementation
+"""
