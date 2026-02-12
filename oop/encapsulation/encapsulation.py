76684e0be9336e548f9fd5311b45f61c8dfaeda4
LorenzoSansone <lorenzo.sansone3@studio.unibo.it>
2026-02-12 01:58:45 +0100

    encapsulation notes

--- oop/encapsulation/encapsulation.py
+++ oop/encapsulation/encapsulation.py
@@ -2,6 +2,8 @@
 #It means keeping data (properties) and methods together in a class, while controlling how the data can be accessed from outside the class.
 #This prevents accidental changes to your data and hides the internal details of how your class works.
 
+
+#PRIVATE ATTRIBUTE
 class Person:
   def __init__(self, name, age):
     self.name = name
@@ -10,3 +12,27 @@ class Person:
 p1 = Person("Emil", 25)
 print(p1.name)
 print(p1.__age) # This will cause an error
+
+
+#PRIVATE METHOD
+class BankAccount:
+    def __init__(self):
+        self.balance = 1000
+
+    def _show_balance(self):
+        print(f"Balance: ₹{self.balance}")  # Protected method
+
+    def __update_balance(self, amount):
+        self.balance += amount             # Private method
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__update_balance(amount)  # Accessing private method internally
+            self._show_balance()           # Accessing protected method
+        else:
+            print("Invalid deposit amount!")
+            
+account = BankAccount()
+account._show_balance()      # Works, but should be treated as internal
+# account.__update_balance(500)  # Error: private method
+account.deposit(500)         # Uses both methods internally
